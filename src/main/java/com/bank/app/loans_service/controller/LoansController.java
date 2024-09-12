993bb1345f1fa99728b41cfb90ff61409e9c32fc
Hasrat Parwez <91671497+hasratparwez9162@users.noreply.github.com>
2024-09-12 15:17:18 +0530

    Add send message login in controller

--- src/main/java/com/bank/app/loans_service/controller/LoansController.java
+++ src/main/java/com/bank/app/loans_service/controller/LoansController.java
@@ -1,6 +1,8 @@
 package com.bank.app.loans_service.controller;
 
 import com.bank.app.loans_service.entity.Loan;
+import com.bank.app.loans_service.exception.ResourceNotFoundException;
+import com.bank.app.loans_service.service.Impl.LoanEventProducer;
 import com.bank.app.loans_service.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +18,14 @@ public class LoansController {
 
         @Autowired
         private LoanService loanService;
+        @Autowired
+        LoanEventProducer loanEventProducer;
 
         @PostMapping("/issue")
         public ResponseEntity<Loan> issueLoan(@RequestBody Loan loan) {
             Loan newLoan = loanService.issueLoan(loan);
+            // Send message after loan issue.
+            loanEventProducer.sendLoanIssuedMessage(newLoan);
             return new ResponseEntity<>(newLoan, HttpStatus.CREATED);
         }
 
@@ -32,13 +38,24 @@ public class LoansController {
         @PutMapping("/status/{loanId}")
         public ResponseEntity<Loan> updateLoanStatus(@PathVariable Long loanId, @RequestParam String status) {
             Loan updatedLoan = loanService.updateLoanStatus(loanId, status);
+            // Send message after update loan
+            loanEventProducer.sendLoanStatusUpdatedMessage(updatedLoan);
             return new ResponseEntity<>(updatedLoan, HttpStatus.OK);
         }
 
         @PutMapping("/repay/{loanId}")
         public ResponseEntity<Loan> repayLoan(@PathVariable Long loanId, @RequestParam BigDecimal amount) {
             Loan repaidLoan = loanService.repayLoan(loanId, amount);
+            // Send confirmation message for repay
+            loanEventProducer.sendLoanRepaidMessage(repaidLoan);
             return new ResponseEntity<>(repaidLoan, HttpStatus.OK);
+        }
+        @PutMapping("/update/loan")
+        public ResponseEntity<Loan> updateLoanDetails(@RequestBody Loan loan) throws ResourceNotFoundException {
+            Loan updatedLoan = loanService.updateLoan(loan);
+            // Send message with Update details of loan
+            loanEventProducer.sendLoanUpdatedMessage(updatedLoan);
+            return new ResponseEntity<>(updatedLoan,HttpStatus.OK);
         }
 
 }

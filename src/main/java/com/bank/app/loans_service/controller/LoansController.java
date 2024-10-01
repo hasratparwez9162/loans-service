364f958226584ee4487dd38dbd0517aec6acc635
Hasrat Parwez <91671497+hasratparwez9162@users.noreply.github.com>
2024-10-01 11:39:17 +0530

    Maintain and update controller

--- src/main/java/com/bank/app/loans_service/controller/LoansController.java
+++ src/main/java/com/bank/app/loans_service/controller/LoansController.java
@@ -4,10 +4,12 @@ import com.bank.app.loans_service.entity.Loan;
 import com.bank.app.loans_service.exception.ResourceNotFoundException;
 import com.bank.app.loans_service.service.Impl.LoanEventProducer;
 import com.bank.app.loans_service.service.LoanService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -22,7 +24,7 @@ public class LoansController {
         LoanEventProducer loanEventProducer;
 
         @PostMapping("/issue")
-        public ResponseEntity<Loan> issueLoan(@RequestBody Loan loan) {
+        public ResponseEntity<Loan> issueLoan(@RequestBody Loan loan) throws JsonProcessingException {
             Loan newLoan = loanService.issueLoan(loan);
             // Send message after loan issue.
             loanEventProducer.sendLoanIssuedMessage(newLoan);
@@ -36,22 +38,22 @@ public class LoansController {
         }
 
         @PutMapping("/status/{loanId}")
-        public ResponseEntity<Loan> updateLoanStatus(@PathVariable Long loanId, @RequestParam String status) {
+        public ResponseEntity<Loan> updateLoanStatus(@PathVariable Long loanId, @RequestParam String status) throws JsonProcessingException {
             Loan updatedLoan = loanService.updateLoanStatus(loanId, status);
-            // Send message after update loan
+            // Send message after update loan status
             loanEventProducer.sendLoanStatusUpdatedMessage(updatedLoan);
             return new ResponseEntity<>(updatedLoan, HttpStatus.OK);
         }
 
         @PutMapping("/repay/{loanId}")
-        public ResponseEntity<Loan> repayLoan(@PathVariable Long loanId, @RequestParam BigDecimal amount) {
+        public ResponseEntity<Loan> repayLoan(@PathVariable Long loanId, @RequestParam BigDecimal amount) throws JsonProcessingException {
             Loan repaidLoan = loanService.repayLoan(loanId, amount);
             // Send confirmation message for repay
             loanEventProducer.sendLoanRepaidMessage(repaidLoan);
             return new ResponseEntity<>(repaidLoan, HttpStatus.OK);
         }
         @PutMapping("/update/loan")
-        public ResponseEntity<Loan> updateLoanDetails(@RequestBody Loan loan) throws ResourceNotFoundException {
+        public ResponseEntity<Loan> updateLoanDetails(@RequestBody Loan loan) throws ResourceNotFoundException, JsonProcessingException {
             Loan updatedLoan = loanService.updateLoan(loan);
             // Send message with Update details of loan
             loanEventProducer.sendLoanUpdatedMessage(updatedLoan);
--- src/main/java/com/bank/app/loans_service/service/Impl/LoanEventProducer.java
+++ src/main/java/com/bank/app/loans_service/service/Impl/LoanEventProducer.java
@@ -1,27 +1,48 @@
 package com.bank.app.loans_service.service.Impl;
 
+import com.bank.app.loans_service.entity.Loan;
+import com.bank.core.entity.LoanNotification;
+import com.bank.core.entity.LoanType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class LoanEventProducer {
     private static final String TOPIC = "loan-service-topic";
 
+
+
     @Autowired
-    private KafkaTemplate<String, Object> kafkaTemplate;
+    private ObjectMapper objectMapper;
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
 
-    public void sendLoanIssuedMessage(Object message) {
-        kafkaTemplate.send(TOPIC, "loan_issued", message);
+    public void sendLoanIssuedMessage(Loan loan)  {
+        LoanNotification loanNotification = new LoanNotification();
+        BeanUtils.copyProperties(loan,loanNotification);
+        kafkaTemplate.send(TOPIC,"loan_issued", loanNotification);
     }
 
-    public void sendLoanStatusUpdatedMessage(Object message) {
-        kafkaTemplate.send(TOPIC, "loan_status_updated", message);
+    public void sendLoanStatusUpdatedMessage(Loan loan)  {
+        LoanNotification loanNotification = new LoanNotification();
+        BeanUtils.copyProperties(loan,loanNotification);
+        kafkaTemplate.send(TOPIC, "loan_status_updated",loanNotification);
     }
 
-    public void sendLoanRepaidMessage(Object message) {
-        kafkaTemplate.send(TOPIC, "loan_repaid", message);
+    public void sendLoanRepaidMessage(Loan loan)  {
+        LoanNotification loanNotification = new LoanNotification();
+        BeanUtils.copyProperties(loan,loanNotification);
+        kafkaTemplate.send(TOPIC,"loan_repaid", loanNotification);
     }
 
-    public void sendLoanUpdatedMessage(Object message) {
-        kafkaTemplate.send(TOPIC, "loan_details_updated", message);
+    public void sendLoanUpdatedMessage(Loan loan)  {
+        LoanNotification loanNotification = new LoanNotification();
+        BeanUtils.copyProperties(loan,loanNotification);
+        kafkaTemplate.send(TOPIC,"loan_details_updated", loanNotification);
     }
 }
